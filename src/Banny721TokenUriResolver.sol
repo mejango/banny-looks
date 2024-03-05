85368b1024aab7714736b87d33c6de2084ac11c9
mejango <mejango@protonmail.com>
2024-03-05 16:17:59 -0300

    refactored outfit contents

--- src/Banny721TokenUriResolver.sol
+++ src/Banny721TokenUriResolver.sol
@@ -15,6 +15,19 @@ contract Banny721TokenUriResolver is IJB721TokenUriResolver, Ownable {
     event SetSvgHash(uint256 indexed tierId, bytes32 indexed svgHash, address caller);
     event SetSvgBaseUri(string baseUri, address caller);
 
+    error UNRECOGNIZED_WORLD();
+    error UNAUTHORIZED_NAKED_BANNY();
+    error UNAUTHORIZED_WORLD();
+    error UNAUTHORIZED_OUTFIT();
+    error UNRECOGNIZED_CATEGORY();
+    error UNRECOGNIZED_OUTFIT();
+    error UNORDERED_CATEGORIES();
+    error CONTENTS_ALREADY_STORED();
+    error HASH_NOT_FOUND();
+    error CONTENTS_MISMATCH();
+    error HASH_ALREADY_STORED();
+    error UNRECOGNIZED_TIER();
+
     string public constant NAKED_BANNY =
         '<g class="a1"><path d="M173 53h4v17h-4z"/></g><g class="a2"><path d="M167 57h3v10h-3z"/><path d="M169 53h4v17h-4z"/></g><g class="a3"><path d="M167 53h3v4h-3z"/><path d="M163 57h4v10h-4z"/><path d="M167 67h3v3h-3z"/></g><g class="b1"><path d="M213 253h-3v-3-3h-3v-7-3h-4v-10h-3v-7-7-3h-3v-73h-4v-10h-3v-10h-3v-7h-4v-7h-3v-3h-3v-3h-4v10h4v10h3v10h3v3h4v7 3 70 3h3v7h3v20h4v7h3v3h3v3h4v4h3v3h3v-3-4z"/><path d="M253 307v-4h-3v-3h-3v-3h-4v-4h-3v-3h-3v-3h-4v-4h-3v-3h-3v-3h-4v-4h-3v-6h-3v-7h-4v17h4v3h3v3h3 4v4h3v3h3v3h4v4h3v3h3v3h4v4h3v3h3v3h4v-6h-4z"/></g><g class="b2"><path d="M250 310v-3h-3v-4h-4v-3h-3v-3h-3v-4h-4v-3h-3v-3h-3v-4h-7v-3h-3v-3h-4v-17h-3v-3h-3v-4h-4v-3h-3v-3h-3v-7h-4v-20h-3v-7h-3v-73-3-7h-4v-3h-3v-10h-3v-10h-4V70h-3v-3l-3 100 3-100v40h-3v10h-4v6h-3v14h-3v3 13h-4v44h4v16h3v14h3v13h4v10h3v7h3v3h4v3h3v4h3v3h4v3h3v4h3v3h4v3h3v7h7v7h6v3h7v3h7v4h13v3h3v3h10v-3h-3zm-103-87v-16h3v-10h-3v6h-4v17h-3v10h3v-7h4z"/><path d="M143 230h4v7h-4zm4 10h3v3h-3zm3 7h3v3h-3zm3 6h4v4h-4z"/><path d="M163 257h-6v3h3v3h3v4h4v-4-3h-4v-3z"/></g><g class="b3"><path d="M143 197v6h4v-6h6v-44h4v-16h3v-14h3v-6h4v-10h3V97h-7v6h-3v4h-3v3h-4v3h-3v4 3h-3v3 4h-4v10h-3v16 4h-3v46h3v-6h3z"/><path d="M140 203h3v17h-3z"/><path d="M137 220h3v10h-3z"/><path d="M153 250h-3v-7h-3v-6h-4v-7h-3v10h3v7h4v6h3v4h3v-7zm-3 10h3v7h-3z"/><path d="M147 257h3v3h-3zm6 0h4v3h-4z"/><path d="M160 263v-3h-3v3 7h6v-7h-3zm-10-56v16h-3v7h3v10h3v7h4v6h6v4h7v-4-3h-3v-10h-4v-13h-3v-14h-3v-16h-4v10h-3z"/><path d="M243 313v-3h-3v-3h-10-3v-4h-7v-3h-7v-3h-6v-7h-7v-7h-3v-3h-4v-3h-3v-4h-3v-3h-4v-3h-3v-4h-3v-3h-4v-3h-3v10h-3v3h-4v3h-3v7h3v7h4v6h3v5h4v3h6v3h3v3h4 3v3h3 4v3h3 3v4h10v3h7 7 3v3h10 3v-3h10v-3h4v-4h-14z"/></g><g class="b4"><path d="M183 130h4v7h-4z"/><path d="M180 127h3v3h-3zm-27-4h4v7h-4z"/><path d="M157 117h3v6h-3z"/><path d="M160 110h3v7h-3z"/><path d="M163 107h4v3h-4zm-3 83h3v7h-3z"/><path d="M163 187h4v3h-4zm20 0h7v3h-7z"/><path d="M180 190h3v3h-3zm10-7h3v4h-3z"/><path d="M193 187h4v6h-4zm-20 53h4v7h-4z"/><path d="M177 247h3v6h-3z"/><path d="M180 253h3v7h-3z"/><path d="M183 260h7v3h-7z"/><path d="M190 263h3v4h-3zm0-20h3v4h-3z"/><path d="M187 240h3v3h-3z"/><path d="M190 237h3v3h-3zm13 23h4v3h-4z"/><path d="M207 263h3v7h-3z"/><path d="M210 270h3v3h-3zm-10 7h3v6h-3z"/><path d="M203 283h4v7h-4z"/><path d="M207 290h6v3h-6z"/></g><g class="o"><path d="M133 157h4v50h-4zm0 63h4v10h-4zm27-163h3v10h-3z"/><path d="M163 53h4v4h-4z"/><path d="M167 50h10v3h-10z"/><path d="M177 53h3v17h-3z"/><path d="M173 70h4v27h-4zm-6 0h3v27h-3z"/><path d="M163 67h4v3h-4zm0 30h4v3h-4z"/><path d="M160 100h3v3h-3z"/><path d="M157 103h3v4h-3z"/><path d="M153 107h4v3h-4z"/><path d="M150 110h3v3h-3z"/><path d="M147 113h3v7h-3z"/><path d="M143 120h4v7h-4z"/><path d="M140 127h3v10h-3z"/><path d="M137 137h3v20h-3zm56-10h4v10h-4z"/><path d="M190 117h3v10h-3z"/><path d="M187 110h3v7h-3z"/><path d="M183 103h4v7h-4z"/><path d="M180 100h3v3h-3z"/><path d="M177 97h3v3h-3zm-40 106h3v17h-3zm0 27h3v10h-3zm10 30h3v7h-3z"/><path d="M150 257v-4h-3v-6h-4v-7h-3v10h3v10h4v-3h3z"/><path d="M150 257h3v3h-3z"/><path d="M163 273v-3h-6v-10h-4v7h-3v3h3v3h4v7h3v-7h3z"/><path d="M163 267h4v3h-4z"/><path d="M170 257h-3-4v3h4v7h3v-10z"/><path d="M157 253h6v4h-6z"/><path d="M153 247h4v6h-4z"/><path d="M150 240h3v7h-3z"/><path d="M147 230h3v10h-3zm13 50h3v7h-3z"/><path d="M143 223h4v7h-4z"/><path d="M147 207h3v16h-3z"/><path d="M150 197h3v10h-3zm-10 0h3v6h-3zm50 113h7v3h-7zm23 10h17v3h-17z"/><path d="M230 323h13v4h-13z"/><path d="M243 320h10v3h-10z"/><path d="M253 317h4v3h-4z"/><path d="M257 307h3v10h-3z"/><path d="M253 303h4v4h-4z"/><path d="M250 300h3v3h-3z"/><path d="M247 297h3v3h-3z"/><path d="M243 293h4v4h-4z"/><path d="M240 290h3v3h-3z"/><path d="M237 287h3v3h-3z"/><path d="M233 283h4v4h-4z"/><path d="M230 280h3v3h-3z"/><path d="M227 277h3v3h-3z"/><path d="M223 273h4v4h-4z"/><path d="M220 267h3v6h-3z"/><path d="M217 260h3v7h-3z"/><path d="M213 253h4v7h-4z"/><path d="M210 247h3v6h-3z"/><path d="M207 237h3v10h-3z"/><path d="M203 227h4v10h-4zm-40 60h4v6h-4zm24 20h3v3h-3z"/><path d="M167 293h3v5h-3zm16 14h4v3h-4z"/><path d="M170 298h4v3h-4zm10 6h3v3h-3z"/><path d="M174 301h6v3h-6zm23 12h6v4h-6z"/><path d="M203 317h10v3h-10zm-2-107v-73h-4v73h3v17h3v-17h-2z"/></g>';
     string public constant DEFAULT_LEGS =
@@ -187,7 +200,7 @@ contract Banny721TokenUriResolver is IJB721TokenUriResolver, Ownable {
             }
 
             // If the tier's category is greater than the last expected category, use the default base URI of the 721 contract. Otherwise use the SVG URI.
-            string baseUri = tier.category > MISC_CATEGORY ? IJB721TiersHook(hook).baseURI() : svgBaseUri;
+            string memory baseUri = tier.category > MISC_CATEGORY ? IJB721TiersHook(hook).baseURI() : svgBaseUri;
 
             // Fallback to returning an IPFS hash if present.
             return JBIpfsDecoder.decode(
@@ -204,97 +217,24 @@ contract Banny721TokenUriResolver is IJB721TokenUriResolver, Ownable {
             outfitIds = _outfitIds;
         } catch (bytes memory) {}
 
-        // Get a reference to the number of outfits are on the Naked Banny.
-        uint256 numberOfOutfits = outfitIds.length;
-
         // Add the world if needed.
         if (worldId != 0) contents = string.concat(contents, svgContentsOf(hook, worldId));
 
         // Start with the Naked Banny.
         contents = string.concat(contents, _nakedBannySvgOf(tier.id));
 
-        // Keep a reference to the outfit ID being iterated on.
-        uint256 outfitId;
+        // Get the outfit contents.
+        string memory outfitContents = _outfitContentsFor({ hook: hook, nakedBannyTier: tier.id, outfitIds: outfitIds });
 
-        // Keep a reference to the outfit's tier being iterated on.
-        JB721Tier memory outfitTier;
-
-        // Keep a reference to the category of the outfit being iterated on.
-        uint256 category;
-
-        // Keep a reference to if certain accessories have been added.
-        bool hasLegs;
-        bool hasNecklace;
-        bool hasFace;
-        bool hasEyes;
-        bool hasMouth;
-
-        // If there are less than 3 outfits, loop once more to make sure all default outfits are added.
-        uint256 numberOfIterations = numberOfOutfits < 3 ? numberOfOutfits + 1 : numberOfOutfits;
-
-        // For each outfit, add the SVG layer if it's owned by the same owner as the Naked Banny being dressed.
-        for (uint256 i; i < numberOfIterations; i++) {
-            // If the outfit is within the bounds of the number of outfits there are, add it normally.
-            if (i < numberOfOutfits) {
-                // Set the outfit ID being iterated on.
-                outfitId = outfitIds[i];
-
-                // Set the tier of the outfit
-                outfitTier = IJB721TiersHook(hook).STORE().tierOfTokenId(hook, outfitId, false);
-
-                // Set the outfit
-                category = outfitTier.category;
-            } else {
-                // Set the category to be greater than the last default category.
-                category = FACE_CATEGORY + 1;
-                outfitId = 0;
-            }
-
-            // Set default legs, necklace, and face if needed.
-            if (category == LEGS_CATEGORY) {
-                hasLegs = true;
-            } else if (category > LEGS_CATEGORY && !hasLegs) {
-                contents = string.concat(contents, DEFAULT_LEGS);
-                hasLegs = true;
-            }
-            if (category == NECKLACE_CATEGORY) {
-                hasNecklace = true;
-            } else if (category > NECKLACE_CATEGORY && !hasNecklace) {
-                contents = string.concat(contents, DEFAULT_NECKLACE);
-                hasNecklace = true;
-            }
-            if (category == FACE_CATEGORY) {
-                hasFace = true;
-            } else if (category > FACE_CATEGORY && !hasFace) {
-                if (category == EYES_CATEGORY) {
-                    hasEyes = true;
-                } else if (category > EYES_CATEGORY && !hasEyes) {
-                    if (tier.id == ALIEN_TIER) contents = string.concat(contents, DEFAULT_ALIEN_EYES);
-                    else contents = string.concat(contents, DEFAULT_STANDARD_EYES);
-                    hasEyes = true;
-                }
-                if (category == MOUTH_CATEGORY) {
-                    hasMouth = true;
-                } else if (category > MOUTH_CATEGORY && !hasMouth) {
-                    contents = string.concat(contents, DEFAULT_MOUTH);
-                    hasMouth = true;
-                }
-
-                if (hasEyes && hasMouth && !hasFace) {
-                    hasFace = true;
-                }
-            }
-
-            // Add the outfit if needed.
-            if (outfitId != 0) {
-                contents = string.concat(contents, svgContentsOf(hook, outfitId));
-            }
+        // Add the outfit contents if there are any.
+        if (bytes(outfitContents).length != 0) {
+            contents = string.concat(contents, outfitContents);
         }
 
         // Return the SVG.
         return _layeredSvg(contents);
     }
-
+    
     /// @param owner The owner allowed to add SVG files that correspond to tier IDs.
     constructor(address owner) Ownable(owner) {}
 
@@ -315,18 +255,18 @@ contract Banny721TokenUriResolver is IJB721TokenUriResolver, Ownable {
         external
     {
         // Make sure call is being made by owner of Naked Banny.
-        if (IERC721(hook).ownerOf(nakedBannyId) != msg.sender) revert();
+        if (IERC721(hook).ownerOf(nakedBannyId) != msg.sender) revert UNAUTHORIZED_NAKED_BANNY();
 
         // Add the world if needed.
         if (worldId != 0) {
             // Check if the owner matched.
-            if (IERC721(hook).ownerOf(worldId) != msg.sender) revert();
+            if (IERC721(hook).ownerOf(worldId) != msg.sender) revert UNAUTHORIZED_WORLD();
 
             // Get the world's tier.
             JB721Tier memory worldTier = IJB721TiersHook(hook).STORE().tierOfTokenId(hook, worldId, false);
 
             // Tier must exist
-            if (worldTier.id == 0) revert();
+            if (worldTier.id == 0) revert UNRECOGNIZED_WORLD();
 
             // Store the world for the banny.
             _attachedWorldIdOf[nakedBannyId] = worldId;
@@ -352,19 +292,19 @@ contract Banny721TokenUriResolver is IJB721TokenUriResolver, Ownable {
             outfitId = outfitIds[i];
 
             // Check if the owner matched.
-            if (IERC721(hook).ownerOf(outfitId) != msg.sender) revert();
+            if (IERC721(hook).ownerOf(outfitId) != msg.sender) revert UNAUTHORIZED_OUTFIT();
 
             // Get the outfit's tier.
             outfitTier = IJB721TiersHook(hook).STORE().tierOfTokenId(hook, outfitId, false);
 
             // Tier must exist
-            if (outfitTier.id == 0) revert();
+            if (outfitTier.id == 0) revert UNRECOGNIZED_OUTFIT();
 
             // The tier's category must be a known category.
-            if (outfitTier.category < LEGS_CATEGORY || outfitTier.category > MISC_CATEGORY) revert();
+            if (outfitTier.category < LEGS_CATEGORY || outfitTier.category > MISC_CATEGORY) revert UNRECOGNIZED_CATEGORY();
 
             // Make sure the category is an increment of the previous outfit's category.
-            if (i != 0 && outfitTier.category <= lastAssetCategory) revert();
+            if (i != 0 && outfitTier.category <= lastAssetCategory) revert UNORDERED_CATEGORIES();
 
             // Keep a reference to the last outfit's category.
             lastAssetCategory = outfitTier.category;
@@ -381,16 +321,16 @@ contract Banny721TokenUriResolver is IJB721TokenUriResolver, Ownable {
     /// @param svgContents The svg contents being stored, not including the parent <svg></svg> element.
     function setSvgContentsOf(uint256 tierId, string calldata svgContents) external {
         // Make sure there isn't already contents for the specified tierId;
-        if (bytes(_svgContentsOf[tierId]).length != 0) revert();
+        if (bytes(_svgContentsOf[tierId]).length != 0) revert CONTENTS_ALREADY_STORED();
 
         // Get the stored svg hash for the tier.
         bytes32 svgHash = svgHashOf[tierId];
 
         // Make sure a hash exists.
-        if (svgHash == bytes32(0)) revert();
+        if (svgHash == bytes32(0)) revert HASH_NOT_FOUND();
 
         // Make sure the content matches the hash.
-        if (keccak256(abi.encodePacked(svgContents)) != svgHash) revert();
+        if (keccak256(abi.encodePacked(svgContents)) != svgHash) revert CONTENTS_MISMATCH();
 
         // Store the svg contents.
         _svgContentsOf[tierId] = svgContents;
@@ -404,7 +344,7 @@ contract Banny721TokenUriResolver is IJB721TokenUriResolver, Ownable {
     /// @param svgHash The svg hash being stored, not including the parent <svg></svg> element.
     function setSvgHashOf(uint256 tierId, bytes32 svgHash) external onlyOwner {
         // Make sure there isn't already contents for the specified tierId;
-        if (svgHashOf[tierId] != bytes32(0)) revert();
+        if (svgHashOf[tierId] != bytes32(0)) revert HASH_ALREADY_STORED();
 
         // Store the svg contents.
         svgHashOf[tierId] = svgHash;
@@ -413,7 +353,7 @@ contract Banny721TokenUriResolver is IJB721TokenUriResolver, Ownable {
     }
 
     /// @notice Allows the owner of this contract to specify the base of the domain hosting the SVG files.
-    function setSvgBaseUriOf(uint256 baseUri) external onlyOwner {
+    function setSvgBaseUriOf(string calldata baseUri) external onlyOwner {
         // Store the base URI.
         svgBaseUri = baseUri;
 
@@ -534,6 +474,87 @@ contract Banny721TokenUriResolver is IJB721TokenUriResolver, Ownable {
             );
         }
 
-        revert();
+        revert UNRECOGNIZED_TIER();
+    }
+
+    /// @notice The SVG contents for a list of outfit IDs.
+    /// @param hook The address of the hook storing the assets.
+    /// @param nakedBannyTier The tier of the naked banny being dressed.
+    /// @param outfitIds The IDs of the outfits that'll be associated with the specified banny.
+    function _outfitContentsFor(address hook, uint256 nakedBannyTier, uint256[] memory outfitIds) internal view returns (string memory contents) {
+        // Get a reference to the number of outfits are on the Naked Banny.
+        uint256 numberOfOutfits = outfitIds.length;
+
+        // Keep a reference to the outfit ID being iterated on.
+        uint256 outfitId;
+
+        // Keep a reference to the category of the outfit being iterated on.
+        uint256 category;
+
+        // Keep a reference to if certain accessories have been added.
+        bool hasLegs;
+        bool hasNecklace;
+        bool hasFace;
+        bool hasEyes;
+        bool hasMouth;
+
+        // If there are less than 3 outfits, loop once more to make sure all default outfits are added.
+        uint256 numberOfIterations = numberOfOutfits < 3 ? numberOfOutfits + 1 : numberOfOutfits;
+
+        // For each outfit, add the SVG layer if it's owned by the same owner as the Naked Banny being dressed.
+        for (uint256 i; i < numberOfIterations; i++) {
+            // If the outfit is within the bounds of the number of outfits there are, add it normally.
+            if (i < numberOfOutfits) {
+                // Set the outfit ID being iterated on.
+                outfitId = outfitIds[i];
+
+                // Set the category of the outfit being iterated on.
+                category = IJB721TiersHook(hook).STORE().tierOfTokenId(hook, outfitId, false).category;
+            } else {
+                // Set the category to be greater than the last default category.
+                category = MOUTH_CATEGORY + 1;
+                outfitId = 0;
+            }
+
+            // Set default legs, necklace, and face if needed.
+            if (category == LEGS_CATEGORY) {
+                hasLegs = true;
+            } else if (category > LEGS_CATEGORY && !hasLegs) {
+                contents = string.concat(contents, DEFAULT_LEGS);
+                hasLegs = true;
+            }
+            if (category == NECKLACE_CATEGORY) {
+                hasNecklace = true;
+            } else if (category > NECKLACE_CATEGORY && !hasNecklace) {
+                contents = string.concat(contents, DEFAULT_NECKLACE);
+                hasNecklace = true;
+            }
+            if (category == FACE_CATEGORY) {
+                hasFace = true;
+            } else if (category > FACE_CATEGORY && !hasFace) {
+                if (category == EYES_CATEGORY) {
+                    hasEyes = true;
+                } else if (category > EYES_CATEGORY && !hasEyes) {
+                    if (nakedBannyTier == ALIEN_TIER) contents = string.concat(contents, DEFAULT_ALIEN_EYES);
+                    else contents = string.concat(contents, DEFAULT_STANDARD_EYES);
+                    hasEyes = true;
+                }
+                if (category == MOUTH_CATEGORY) {
+                    hasMouth = true;
+                } else if (category > MOUTH_CATEGORY && !hasMouth) {
+                    contents = string.concat(contents, DEFAULT_MOUTH);
+                    hasMouth = true;
+                }
+
+                if (hasEyes && hasMouth && !hasFace) {
+                    hasFace = true;
+                }
+            }
+
+            // Add the outfit if needed.
+            if (outfitId != 0) {
+                contents = string.concat(contents, svgContentsOf(hook, outfitId));
+            }
+        }
     }
 }
