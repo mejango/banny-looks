e4f6748920452167fd3dc6e6e5ecb9b69bd3bcb4
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2024-09-24 01:25:52 +0200

    ci: bump package version

--- script/helpers/BannyverseDeploymentLib.sol
+++ script/helpers/BannyverseDeploymentLib.sol
@@ -49,14 +49,11 @@ library BannyverseDeploymentLib {
         );
 
         // TODO: Update the following after every deploy.
-        deployment.revnetId = 4;
-        // if (keccak256(abi.encode(network_name)) == keccak256(abi.encode("sepolia"))) {
-        //     deployment.revnetId = 7;
-        // } else if (keccak256(abi.encode(network_name)) == keccak256(abi.encode("optimism_sepolia"))) {
-        //     deployment.revnetId = 7;
-        // } else {
-        //     deployment.revnetId = 2;
-        // }
+        if (keccak256(abi.encode(network_name)) == keccak256(abi.encode("sepolia"))) {
+            deployment.revnetId = 23;
+        } else {
+            deployment.revnetId = 5;
+        }
     }
 
     /// @notice Get the address of a contract that was deployed by the Deploy script.
